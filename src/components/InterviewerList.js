dd5f91ced443bf1e62d1dc7a0a5fed1cc777cf33
Mitali <mitali.kawde11@gmail.com>
2023-07-18 20:01:05 +0530

    Refactoring Interviewer components to reduce the number of props (id)

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -8,11 +8,10 @@ export default function InterviewerList(props) {
     return (
       <InterviewerListItem 
         key={interviewer.id}
-        id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer}
-        setInterviewer={props.setInterviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
       />
     )
   });
@@ -25,5 +24,5 @@ export default function InterviewerList(props) {
       </ul>
     </section>
   );
-  
+
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -9,7 +9,7 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
+    <li onClick={props.setInterviewer} className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
@@ -18,5 +18,5 @@ export default function InterviewerListItem(props) {
       {props.selected && props.name}
     </li>
   );
-  
+
 };
