ccd7e5b3f993586deaf16283c65df4487313563b
Mitali <mitali.kawde11@gmail.com>
2023-07-18 18:08:27 +0530

    building InterviewerList component to represent one interviewer

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classNames from 'classnames';
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
+  return (
+    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+        // src="https://i.imgur.com/LpaY82x.png"
+        // alt="Sylvia Palmer"
+      />
+      {props.selected && props.name}
+    </li>
+  );
+};
