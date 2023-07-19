ad5e3e63a845d32ec34e0a40f1e61b1935312ad4
Mitali <mitali.kawde11@gmail.com>
2023-07-19 08:28:25 +0530

    writing stories(Create & Edit) for appointment Form component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
+import Form from "components/Appointment/Form.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -178,5 +179,21 @@ storiesOf("Appointment", module)
     <Error 
       message="Could not delete appointment"
       onClose={action("onClose")}
+    />
+  ))
+  .add("Edit", () => (
+    <Form 
+      student="Mitali Kawde"
+      interviewer={1}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create", () => (
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ))
