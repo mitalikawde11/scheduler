17fa119fbcfbddc5be491c640218db7d28e7da7d
Mitali <mitali.kawde11@gmail.com>
2023-07-24 22:11:41 +0530

    fetching interviewers data: modifying the effect method to do a interviewers request using Promise.all & updating the interviewers state with response

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,14 +5,15 @@ import DayList from "./DayList";
 import "components/Appointment";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
     
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   
   const setDay = day => setState({ ...state, day });
@@ -21,22 +22,29 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then((all) => {
-       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data }));
+       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+       
     });
   }, []);
-
+  console.log("interviewersss: ", state.interviewers);
   const dailyAppointments = getAppointmentsForDay(state, state.day)
 
-  const appointmentsForDay = dailyAppointments.map((appointment) => {
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    
     return (
       <Appointment 
           key={appointment.id}
-          {...appointment}
+          id={appointment.id}
+          time={appointment.time}
+          interview={interview}
+          // {...appointment}
       />
-    )
-  })
+    );
+  });
   
   return (
     <main className="layout">
@@ -61,8 +69,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {appointmentsForDay}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
