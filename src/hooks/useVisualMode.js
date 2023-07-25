fbaeffa0568ebbf1809c2c2ec11747ed83534e35
Mitali <mitali.kawde11@gmail.com>
2023-07-25 06:52:41 +0530

    building custom hook 'useVisualMode' that allows Appointment component to keep track of the 'modes': initial mode, transition, back, back limit & transition with replace

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+  
+  function transition(newMode, replace = false) {
+    // if replace is true, get rid of last element and add new mode
+    // if replace is false, add new mode
+    setHistory(prev => replace ?  [...prev.slice(0,-1), newMode] :  [...prev, newMode] )
+      
+  };
+
+  
+  function back() {
+    if (history.length === 1) {
+      return;
+    }
+
+    //the slice removes the last element
+    setHistory(prev =>  prev.slice(0,-1))
+  };
+
+
+  return { mode: history[history.length - 1], transition, back };
+};
