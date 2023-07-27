82951ff5784657a9f448cb467e7af4cefa014a83
Mitali <mitali.kawde11@gmail.com>
2023-07-27 17:09:10 +0530

    implementing the save function, showing Status component when mode equal to saving

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,37 +5,49 @@ import Empty from "./Empty";
 import "components/Appointment/styles.scss";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
+import Status from "./Status";
 
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW))
+    
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {/* {props.interview ? 
-        <Show student={props.interview.student} interviewers={props.interview.interviewer} /> : 
-        <Empty />
-      } */}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          interviewers={props.interview.interviewer}
         />
       )}
       {mode === CREATE && (
         <Form 
-          interviewers={[]}
-          onSave={() => console.log("onSave")}
-          onCancel={() => {back()}}
+          interviewers={props.interviewers}
+          onSave={save}
+          onCancel={back}
         />
+      )}
+      {mode === SAVING && (
+        <Status message="Saving" />
       )}
       
     </article>
