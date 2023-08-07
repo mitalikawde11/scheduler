e8399a8e558756e5c1525046183ec85b330a30f7
Mitali <mitali.kawde11@gmail.com>
2023-08-08 00:34:19 +0530

    refactoring updateSpots function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -20,7 +20,6 @@ export default function useApplicationData() {
     ]).then((all) => {
        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
     })
-    // .then(error => console.error(error))
   }, []);
   
   function bookInterview(id, interview) {
@@ -34,7 +33,7 @@ export default function useApplicationData() {
     };
     return axios.put(`/api/appointments/${id}`, {interview})
     .then(() => {
-      updateSpots(id)
+      updateSpots(appointments)
       setState((prev) => {
         return { ...prev, appointments}
       })
@@ -52,30 +51,38 @@ export default function useApplicationData() {
     };
     return axios.delete(`/api/appointments/${id}`)
     .then(() => {
-      updateSpots(id)
+      updateSpots(appointments)
       setState((prev) => {
         return { ...prev, appointments}
       })
     })
   }
 
-  function updateSpots(appointmentId) {
-    let numberOfSpots = 0;
-    if (state.appointments[appointmentId].interview) {
-      numberOfSpots += 1;
-    } else {
-      numberOfSpots += -1;
-    }
-    setState((prev) => {
-      for (const day of prev.days) {
-        if(prev.day === day.name) {
-          day.spots += numberOfSpots;
+  function updateSpots(appointments) {
+    const dayObject = state.days.find(day => {
+      if (day.name === state.day) {
+        return day
+      }
+    })
+    const appointmentArrOfDay = dayObject.appointments;
+    let spots = 0;
+    for (const key in appointments) {
+      const appointment = appointments[key]
+      if (appointmentArrOfDay.includes(appointment.id)) {
+        if (!appointment.interview) {
+          spots += 1;
         }
       }
-      return { ...prev}
-    })
+    }
+    setState((prev) => {
+          for (const day of prev.days) {
+            if(prev.day === day.name) {
+              day.spots = spots;
+            }
+          }
+          return prev;
+        })
   }
-
 
   return { state, setDay, bookInterview, cancelInterview }
 }
